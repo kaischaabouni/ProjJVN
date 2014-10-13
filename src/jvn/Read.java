3a28d67863e6d4d86d23e57b70b43cbc8f430305
Foux01 <alexandre.morjaret@gmail.com>
2014-10-13 09:59:21 +0200

    les annotation + proxy debut

---
+++ src/jvn/Read.java
@@ -0,0 +1,11 @@
+package jvn;
+import java.lang.annotation.*;
+//Is the annotation available at execution time
+@Retention(RetentionPolicy.RUNTIME)
+//Annotation associated with a type (Classe, interface)
+@Target(ElementType.METHOD)
+
+
+public @interface Read {
+
+}
---
+++ src/jvn/Write.java
@@ -0,0 +1,15 @@
+package jvn;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//Is the annotation available at execution time
+@Retention(RetentionPolicy.RUNTIME)
+//Annotation associated with a type (Classe, interface)
+@Target(ElementType.METHOD)
+
+public @interface Write {
+
+}
